43b68e00ca67730ef2681befaac9dd66fc4d79e1
Kim Lauwers <lauwers.kim@gmail.com>
2020-04-17 09:44:20 +0200

    - intitial test added - extra dependencies added - Item: Equals + to String implemented - Item: getters added (not yet used everywhere)

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,21 @@
 package com.gildedrose;
 
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayNameGeneration(ReplaceUnderscores.class)
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
-    }
+    void product_quality_decrease_with_one_selldate_not_overtime() {
+        final Item[] items = new Item[] {new Item("+5 Dexterity Vest", 10, 20)};
 
+        final GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        assertThat(app.items[0]).isEqualTo(new Item("+5 Dexterity Vest", 9, 19));
+    }
 }

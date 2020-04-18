6feb875b2313d3ebbebafb95a73f1c0a2b9eb56d
Kim Lauwers <lauwers.kim@gmail.com>
2020-04-18 09:41:18 +0200

    Refactor: private methods added for more readable code

--- Java/src/main/java/com/gildedrose/domain/calculator/quality/BackstagePassProductQualityCalculator.java
+++ Java/src/main/java/com/gildedrose/domain/calculator/quality/BackstagePassProductQualityCalculator.java
@@ -8,21 +8,27 @@ import static java.lang.Math.min;
 public class BackstagePassProductQualityCalculator implements QualityCalculator {
     private static final int MAXIMUM_QUALITY = 50;
     private static final int MINIMUM_QUALITY = 0;
+    private static final int QUALITY_INCREASE_AMOUNT = 1;
 
     @Override
     public int calculateQualityWithinSellIn(final int currentQuality, final int sellIn) {
         int newQuality = currentQuality;
-        newQuality += 1;
-
-        if (sellIn < 11) {
-            newQuality += 1;
-        }
-
-        if (sellIn < 6) {
-            newQuality += 1;
-        }
-
+        newQuality = increaseQuality(newQuality);
+        newQuality = addExtraQualityWhenSellInWithIn10Days(sellIn, newQuality);
+        newQuality = addExtraQualityWhenSellInWithIn5Days(sellIn, newQuality);
         return min(MAXIMUM_QUALITY, newQuality);
+    }
+
+    private int addExtraQualityWhenSellInWithIn10Days(final int sellIn, int newQuality) {
+        return sellIn < 11 ? increaseQuality(newQuality) : newQuality;
+    }
+
+    private int addExtraQualityWhenSellInWithIn5Days(final int sellIn, int newQuality) {
+        return sellIn < 6 ? increaseQuality(newQuality) : newQuality;
+    }
+
+    private int increaseQuality(int quality) {
+        return quality + QUALITY_INCREASE_AMOUNT;
     }
 
     @Override
